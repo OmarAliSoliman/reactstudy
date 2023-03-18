0963339c143094b07372b0c415e8ee287905ff9f
OmarAliSoliman <omarasmar9955@gmail.com>
2023-03-18 20:30:25 +0200

    counter update

--- redux-toolkit/src/store/index.js
+++ redux-toolkit/src/store/index.js
@@ -3,9 +3,14 @@ import { legacy_createStore as createStore } from "redux";
 const initState = { counter: 0, show: false };
 const countReducer = (state = initState, action) => {
   if (action.type === "increase") {
+    
     return { ...state, counter: state.counter + action.payload };
   }
   if (action.type === "decrease") {
+    // var newS = state.counter - action.payload;
+    // if(newS <=0){
+    //   newS = 0
+    // }
     return { ...state, counter: state.counter - action.payload };
   }
   if (action.type === "toggleShow") {
